9bc07c143df804d2fd29ecbd889282095cd3c2dd
Santiago Arias <santyac27@gmail.com>
2021-03-12 18:38:25 +0100

    Se preapara todo para comenzar a crear componentes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,42 @@
+import { Col, Container, Form, FormControl, Row, Table, ToastHeader } from "react-bootstrap";
+
 function App() {
   return (
     <>
+      <Container fluid as="section" className="principal">
+        <Row as="header" className="cabecera">
+          <Col as="h2">
+            Listado de ingresos
+        </Col>
+        </Row>
+        <main>
+          <Row>
+            <Col as="div" className="info-listado info-listado-top text-right">
+              <Form.Label>
+                Buscar
+              <FormControl type="text" className="form-control-sm" />
+              </Form.Label>
+            </Col>
+          </Row>
+          <Table striped bordered hover className="listado">
+            <thead className="thead-light">
+              <tr>
+                <th className="col-min">Num.</th>
+                <th className="col-med">Fecha</th>
+                <th className="col-concepto">Concepto</th>
+                <th className="col-med">Base</th>
+                <th className="col-max">IVA</th>
+                <th className="col-med">Total</th>
+                <th className="col-max">Estado</th>
+                <th className="col-max">Vence</th>
+              </tr>
+            </thead>
+          </Table>
+        </main>
+      </Container >
+      <div className="loading off">
+        <img src="img/loading.svg" alt="cargando" />
+      </div>
     </>
   );
 }
